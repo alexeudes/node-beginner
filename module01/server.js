61ff37a704b94d8bf95c0714def0e5f2856940b1
Alex Eudes <alex.eudes@gmail.com>
2020-05-18 21:03:05 -0300

    Set port dinamically with command line arguments.

--- module01/server.js
+++ module01/server.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
+const PORT = process.argv[2] || 8000
+
 http.createServer((request, response) => {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Server is running!');
+    response.write('Server!');
     response.end();
-}).listen(8000);
+}).listen(PORT, getPort);
+
+function getPort() {
+    console.log('Up and running server on port: ', PORT);
+}
