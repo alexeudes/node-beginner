9f973f083113cdaf8bba841f128408002a682b42
Alex Eudes <alex.eudes@gmail.com>
2020-05-18 21:19:58 -0300

    Add some routes.

--- module01/server.js
+++ module01/server.js
@@ -2,12 +2,36 @@ const http = require('http');
 
 const PORT = process.argv[2] || 8000
 
-http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Server!');
-    response.end();
-}).listen(PORT, getPort);
+http.createServer(getRequest)
+     .listen(PORT, getPort);
 
 function getPort() {
     console.log('Up and running server on port: ', PORT);
+}
+
+function getRequest(request, response) {
+    let body;
+
+    switch (request.url) {
+        case '/json':
+            response.statusCode = 200;
+            response.setHeader('Content-Type', 'application/json');
+            body = JSON.stringify({
+                id: 1,
+                route: 'json'
+            });
+            break;
+        case '/html':
+            response.setHeader('Content-Type', 'text/html');
+            body = '<h1>HTML</h1>'
+            break;
+        case '/':
+            body = 'HOME';
+            break;
+        default:
+            response.statusCode = 404;
+            body = 'Page Not Found';
+    }
+
+    response.end(body);
 }
